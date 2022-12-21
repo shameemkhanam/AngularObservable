c5b47c850ff3e04eb475ed78a33c04cc80520085
unknown <shameemkhanam@gmail.com>
2022-12-21 23:25:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularObservables';
+
+  myObservable = new Observable((observer) => {
+    console.log('Observable starts..');
+    setTimeout(() => {observer.next('1')}, 1000);
+    setTimeout(() => {observer.next('2')}, 2000);
+    setTimeout(() => {observer.next('3')}, 3000);
+    setTimeout(() => {observer.next('4')}, 4000);
+    setTimeout(() => {observer.next('5')}, 5000);
+    // observer.next('1');
+    // observer.next('2');
+    // observer.next('3');
+    // observer.next('4');
+    // observer.next('5');
+  });
+
+  ngOnInit() {
+    this.myObservable.subscribe((val) => {
+      console.log(val);      
+    })
+  }
 }
